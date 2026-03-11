63d939e16cc2f9dfa765486a49564fbb811232bb
Huseyn Rustemli <rstmli08@gmail.com>
2026-03-11 13:17:59 +0400

    update

--- src/main/java/com/gateway4/config/GateWayConfig.java
+++ src/main/java/com/gateway4/config/GateWayConfig.java
@@ -1,4 +1,24 @@
 package com.gateway4.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GateWayConfig {
+  @Bean
+  public RouteLocator customRouter(RouteLocatorBuilder builder) {
+    return builder.routes()
+            .route("book-service", r -> r.path("/book/**")
+                    .filters(f -> f.stripPrefix(1))
+                    .uri("http://localhost:8082"))
+
+            .route("carsalon-service", r ->
+                    r.path("/carsalon/**")
+                            .filters(f -> f.stripPrefix(1))
+                            .uri("http://localhost:8081"))
+            .build();
+  }
+
 }
